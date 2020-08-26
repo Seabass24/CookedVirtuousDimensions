d8afa93efde59f2e7d1a215eac737c5c5f63e49e
Sebastian Rodriguez <spr5501@psu.edu>
2020-08-26 21:58:54 +0000

    Space and period

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 name = input("Enter your name:")
-print(f"Hello , {name}!")
+print(f"Hell, {name}!")
 age = input("Enter your age:")
 age = int(age)
-print(f"You will be {age + 1} next year")
+print(f"You will be {age + 1} next year.")
